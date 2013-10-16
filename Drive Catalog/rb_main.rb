8da623af0e7246fd6e64d8c0e49e7fc2228d2517
opn-src <pierce@PieClient-MacBook-Pro.local>
2013-10-16 14:16:48 -0700

    DONE!!!

--- Drive Catalog/rb_main.rb
+++ Drive Catalog/rb_main.rb
@@ -10,7 +10,8 @@
 # do that here too.
 framework 'Cocoa'
 #$:.unshift '/Users/pierce/.rvm/gems/ruby-2.0.0-p247/gems/sequel-4.2.0/lib' # sequel
-$:.unshift '/Users/pierce/.rvm/gems/ruby-2.0.0-p247/gems/sqlite3-1.3.8/lib' # sqlite
+#$:.unshift '/Users/pierce/.rvm/gems/ruby-2.0.0-p247/gems/sqlite3-1.3.8/lib' # sqlite
+#$:.unshift *Dir['/Library/Application Support/Drive Catalog/Gems/**/lib']
 
 require 'rubygems'
 require 'sqlite3'
@@ -24,6 +25,7 @@ main = File.basename(__FILE__, File.extname(__FILE__))
 dir_path = NSBundle.mainBundle.resourcePath.fileSystemRepresentation
 Dir.glob(File.join(dir_path, '*.{rb,rbo}')).map { |x| File.basename(x, File.extname(x)) }.uniq.each do |path|
   if path != main
+    puts path
     require(path)
   end
 end
